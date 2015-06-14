67980b9148cc64e4f6b9465ee40e97ba8488aa50
Gigitsu <gigitsu.23@gmail.com>
2015-06-14 15:36:32 +0200

    Fixed master hostname issue

--- spark/setup.sh
+++ spark/setup.sh
@@ -1,21 +1,25 @@
 #!/usr/bin/env bash
 
 function sethostname {
-    hn="$(hostname)"
+    hn="$(hostname).cloudapp.net"
 
-    echo "    hostname: $hn.cloudapp.net" >> docker-compose.yml
-    echo "export SPARK_PUBLIC_DNS=\"$hn.cloudapp.net\"" >> conf/spark-env.sh
+    echo "    hostname: $hn" >> ./docker-compose.yml
+    echo "export SPARK_PUBLIC_DNS=\"$hn\"" >> ./conf/spark-env.sh
+
+    if [ "$1" = 'master' ]; then
+        echo "spark.master spark://$hn:7077" >> ./conf/spark-defaults.conf
+    fi
 }
 
 function master {
-    cp master.yml docker-compose.yml
-    cp -r ./conf_master ./conf 
-    sethostname
+    cp ./master.yml ./docker-compose.yml
+    cp -r ./conf_master ./conf
+    sethostname "master"
 }
 
 function worker {
-    cp worker.yml docker-compose.yml
-    cp -r ./conf_worker /conf
+    cp ./worker.yml ./docker-compose.yml
+    cp -r ./conf_worker ./conf
     sethostname
 }
 
@@ -23,7 +27,7 @@ if [ $# -ne 1 ]; then
     echo "You need to type 'master' or 'worker' in order to configure this node"
     exit 1
 else
-    rm -f docker-compose.yml
+    rm -f ./docker-compose.yml
     rm -rf ./conf
     if [ "$1" = 'master' ]; then
         master
