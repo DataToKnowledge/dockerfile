8af5f998611184a7c6d1e36d3064eb66681084fa
Gigitsu <gigitsu.23@gmail.com>
2015-06-14 15:08:44 +0200

    Added dns option

--- spark/setup.sh
+++ spark/setup.sh
@@ -4,15 +4,18 @@ function sethostname {
     hn="$(hostname)"
 
     echo "    hostname: $hn.cloudapp.net" >> docker-compose.yml
+    echo "export SPARK_PUBLIC_DNS=\"$hn.cloudapp.net\"" >> conf/spark-env.sh
 }
 
 function master {
     cp master.yml docker-compose.yml
+    cp -r ./conf_master ./conf 
     sethostname
 }
 
 function worker {
     cp worker.yml docker-compose.yml
+    cp -r ./conf_worker /conf
     sethostname
 }
 
@@ -20,6 +23,8 @@ if [ $# -ne 1 ]; then
     echo "You need to type 'master' or 'worker' in order to configure this node"
     exit 1
 else
+    rm -f docker-compose.yml
+    rm -rf ./conf
     if [ "$1" = 'master' ]; then
         master
     elif [ "$1" = 'worker' ]; then
