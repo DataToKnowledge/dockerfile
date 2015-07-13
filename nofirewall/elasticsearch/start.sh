45032d3d8de573810eb01f39f475fefbb6945862
Gigitsu <gigitsu.23@gmail.com>
2015-07-13 17:36:49 +0200

    Updated start script

--- nofirewall/elasticsearch/start.sh
+++ nofirewall/elasticsearch/start.sh
@@ -13,14 +13,31 @@ EOF
 fi
 
 name=$1
+baseDir="/data"
+esDir="$baseDir/elasticsearch/{es#}"
+dataDir="$esDir/data"
+logsDir="$esDir/logs"
 
 if [[ $name =~ ^-?[0-9]+$ ]]; then
   name="es$name"
 fi
 
-echo $name
+if [ -d "$baseDir" ]; then
+  if [ ! -d "$dataDir" ]; then
+    mkdir -p $dataDir
+  fi
+  if [ ! -d "logsDir" ]; then
+    mkdir -p $logsDir
+  fi
+else
+  echo "$baseDir not exists"
+fi
 
 cp -f ./docker-compose.yml.template ./docker-compose.yml
+
+echo "    - $dataDir:/usr/share/elasticsearch/data" >> ./docker-compose.yml
+echo "    - $logsDir:/usr/share/elasticsearch/logs" >> ./docker-compose.yml
+
 sed -i.bak s/{es#}/$name/g ./docker-compose.yml
 
 #docker-compose up -d
