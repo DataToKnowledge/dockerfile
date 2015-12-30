620477cf4d61e244723dc6ee7d0eb40f523e786a
Fumarola Fabio (UniCredit) <fabio.fumarola@unicredit.eu>
2015-12-30 17:55:34 +0100

    update to kafka 0.9.0.0

---
+++ kafka/entrypoint.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# empty hosts file
+# echo > /etc/hosts
+
+
+# properties to add
+echo "broker.id=${ID}
+advertised.host.name=${NAME}
+zookeeper.connect=${ZOOKEEPERS}" >> /opt/kafka/server.properties
+
+echo " writing properties
+broker.id=${ID}
+advertised.host.name=${NAME}
+zookeeper.connect=${ZOOKEEPERS}"
+
+/opt/kafka/bin/kafka-server-start.sh "$@"
