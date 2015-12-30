a54ac843a68746227c28a64a91dd36b614770200
Fumarola Fabio (UniCredit) <fabio.fumarola@unicredit.eu>
2015-12-30 11:42:28 +0100

    fix error in conf loading and add parameter for maximum cpu

--- spark/conf/spark-env.sh
+++ spark/conf/spark-env.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env bash
 
 # This file is sourced when running various Spark programs.
-
-export SPARK_CONF_DIR: /conf
-
 export SPARK_WORKER_CORES=1
 export SPARK_WORKER_MEMORY=3G
 export SPARK_WORKER_PORT=7078
