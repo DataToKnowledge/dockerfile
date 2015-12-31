4d83832cf7b84b4ff7e50ad0fdc30b093aff9b71
Fumarola Fabio (UniCredit) <fabio.fumarola@unicredit.eu>
2015-12-31 21:02:01 +0100

    made entrypoint executable

--- elasticsearch/docker-entrypoint.sh
+++ elasticsearch/docker-entrypoint.sh
@@ -2,18 +2,6 @@
 
 set -e
 
-# Add elasticsearch as command if needed
-if [ "${1:0:1}" = '-' ]; then
-	set -- elasticsearch "$@"
-fi
-
-# Drop root privileges if we are running elasticsearch
-if [ "$1" = 'elasticsearch' ]; then
-	# Change the ownership of /usr/share/elasticsearch/data to elasticsearch
-	chown -R elasticsearch:elasticsearch /usr/share/elasticsearch/data
-	exec gosu elasticsearch "$@"
-fi
-
 # define a custom elasticsearch.yml
 echo "
 cluster.name: wheretolive
@@ -28,6 +16,18 @@ discovery.zen.fd.ping_interval: 6s
 discovery.zen.fd.ping_retries: 10
 
 " >> /usr/share/elasticsearch/config/elasticsearch.yml
+
+# Add elasticsearch as command if needed
+if [ "${1:0:1}" = '-' ]; then
+	set -- elasticsearch "$@"
+fi
+
+# Drop root privileges if we are running elasticsearch
+if [ "$1" = 'elasticsearch' ]; then
+	# Change the ownership of /usr/share/elasticsearch/data to elasticsearch
+	chown -R elasticsearch:elasticsearch /usr/share/elasticsearch/data
+	exec gosu elasticsearch "$@"
+fi
 
 # As argument is not related to elasticsearch,
 # then assume that user wants to run his own process,
