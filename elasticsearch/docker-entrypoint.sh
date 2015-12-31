308b5767e354a404472be86ce77d3c8241ccb56f
Fumarola Fabio (UniCredit) <fabio.fumarola@unicredit.eu>
2015-12-31 20:48:48 +0100

    update es to 2.1.1

---
+++ elasticsearch/docker-entrypoint.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+set -e
+
+# Add elasticsearch as command if needed
+if [ "${1:0:1}" = '-' ]; then
+	set -- elasticsearch "$@"
+fi
+
+# Drop root privileges if we are running elasticsearch
+if [ "$1" = 'elasticsearch' ]; then
+	# Change the ownership of /usr/share/elasticsearch/data to elasticsearch
+	chown -R elasticsearch:elasticsearch /usr/share/elasticsearch/data
+	exec gosu elasticsearch "$@"
+fi
+
+# define a custom elasticsearch.yml
+echo "
+cluster.name: wheretolive
+node.name: ${NAME}
+bootstrap.mlockall: true
+
+discovery.zen.minimum_master_nodes: 3
+discovery.zen.ping.timeout: 30s
+discovery.zen.ping.unicast.hosts: [${UNICAST_HOSTS}]
+
+discovery.zen.fd.ping_interval: 6s
+discovery.zen.fd.ping_retries: 10
+
+" >> /usr/share/elasticsearch/config/elasticsearch.yml
+
+# As argument is not related to elasticsearch,
+# then assume that user wants to run his own process,
+# for example a `bash` shell to explore this image
+exec "$@"
--- elasticsearch/start.sh
+++ elasticsearch/start.sh
@@ -3,14 +3,14 @@
 #if no parameter is passed to the script, print the usage help
 if [ "$#" -eq 0 ]; then
     cat << 'EOF'
-Usage: start [OPTIONS] INDEX|NAME
+Usage: start [OPTIONS] INDEX
 
 Options:
   -m              If set indicates that is a master node
 
 Example:
-$: start -m 0     -- Creates a master node named esm-0
-$: start node1    -- Creates a data node named node1
+$: start -m 0     -- Creates a master node named es-master-0 [DEPRECATED]
+$: start -d 0 -hosts es-data-0,es-data-1,es-data-2    -- Creates a data node named es-data-0
 EOF
     exit 1
 fi
@@ -23,19 +23,23 @@ spwd=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 master=false; [ "$1" == "-m" ] && master=true
 data=true; [ "$1" == "-m" ] && data=false
 
-#this cycle get the last argument passed to the script
-#and save that in the $name variable, to use for naming the container
-for last; do true; done
-name=$last
+# the second parameter holds the id
+id=$2
 
 #if $name is a number then change it in:
-if [[ $name =~ ^-?[0-9]+$ ]]; then
-  if [ $master == true ]; then #"esm-#" if is master node
-    name="esm-$name"
-  else
-    name="esd-$name" #"esd-#" if is data node
-  fi
+if [[ $id == ^-?[0-9]+$ ]]; then
+  echo "the id should be numeric"
+  exit 1
 fi
+
+if [ $master == true ]; then #"esm-#" if is master node
+  name="es-master-$id"
+else
+  name="es-data-$id" #"esd-#" if is data node
+fi
+
+# unicast hosts
+unicast_hosts=$4
 
 baseDir="/data"
 esDir="$baseDir/elasticsearch"
@@ -60,25 +64,21 @@ fi
 imgName="data2knowledge/elasticsearch:1.6.0"
 
 #copy the template configuration file
-cp -f $configDir/elasticsearch.yml.template $configDir/elasticsearch.yml
+# cp -f $configDir/elasticsearch.yml.template $configDir/elasticsearch.yml
 
 #set the node type, $master and $data can be true or false
-echo "node.master: $master" >> $configDir/elasticsearch.yml
-echo "node.data: $data" >> $configDir/elasticsearch.yml
-
-#replace every {esn#} instance in config file with the actual node name
-sed -i.bak s/{esn#}/$name/g $configDir/elasticsearch.yml
+# echo "node.master: $master" >> $configDir/elasticsearch.yml
+# echo "node.data: $data" >> $configDir/elasticsearch.yml
 
 docker build -t $imgName $spwd
 docker stop $name &> /dev/null
 docker rm $name &> /dev/null
-docker run --name $name --restart on-failure -d \
+
+echo "starting ${name} ..."
+docker run --name $name --restart on-failure -dt \
   -p 9200:9200 \
   -p 9300:9300 \
-  -e ES_MIN_MEM=2g \
-  -e ES_MAX_MEM=2g \
-  -e ES_HEAP_SIZE=2g \
+  -e ES_HEAP_SIZE=3g \
+  -e UNICAST_HOSTS=$unicast_hosts \
   -v $dataDir:/usr/share/elasticsearch/data \
-  -v $logsDir:/usr/share/elasticsearch/logs \
-  -v $configDir:/usr/share/elasticsearch/config \
   $imgName
