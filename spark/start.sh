ecbc677927054424aa0ec3c4092b7847c1158875
Fumarola Fabio (UniCredit) <fabio.fumarola@unicredit.eu>
2016-01-05 18:59:34 +0100

    removed conf dir

--- spark/start.sh
+++ spark/start.sh
@@ -36,11 +36,10 @@ fi
 baseDir="/data"
 sparkDir="$baseDir/spark"
 dataDir="$sparkDir/data"
-confDir="$sparkDir/config"
 logsDir="$sparkDir/logs"
 
 #an array that contains all the needed directories
-paths=($confDir $dataDir $logsDir)
+paths=($dataDir $logsDir)
 
 #if $baseDir exists, then create the other subdirectories
 if [ -d "$baseDir" ]; then
@@ -56,27 +55,21 @@ fi
 
 imgName="data2knowledge/spark:1.6.0"
 
-cp -f $spwd/config/* $confDir/
-
 #build the docker file in the spwd folder
 docker build -t $imgName $spwd
 docker stop $name &> /dev/null
 docker rm $name &> /dev/null
 
 if [ $master == true ]; then
-  echo "spark.deploy.recoveryMode ZOOKEEPER" >> $confDir/spark-default.conf
-  echo "spark.deploy.zookeeper.url zoo-1:2181,zoo-2:2181,zoo-3:2181" >> $confDir/spark-default.conf
   docker run --name $name --restart on-failure -dt \
     -v $dataDir:/tmp/data \
     -v $logsDir:/logs \
-    -v $confDir:/conf \
     $imgName \
     org.apache.spark.deploy.master.Master -h $name
 else
   docker run --name $name --restart on-failure -dt \
     -v $dataDir:/tmp/data \
     -v $logsDir:/logs \
-    -v $confDir:/conf \
     $imgName \
     org.apache.spark.deploy.worker.Worker -h $name spark://spark-master-0:7077,spark-master-1:7077
 fi
