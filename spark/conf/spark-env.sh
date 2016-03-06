79e18210a1a63057229b851e35f3087cffc0043e
Fumarola Fabio (UniCredit) <fabio.fumarola@unicredit.eu>
2016-03-06 11:50:12 +0100

    change worker config

--- spark/conf/spark-env.sh
+++ spark/conf/spark-env.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # This file is sourced when running various Spark programs.
-export SPARK_WORKER_CORES=1
-export SPARK_WORKER_MEMORY=3G
+export SPARK_WORKER_CORES=2
+export SPARK_WORKER_MEMORY=2G
 export SPARK_WORKER_PORT=7078
 export SPARK_MASTER_OPTS="-Dspark.worker.timeout=600"
